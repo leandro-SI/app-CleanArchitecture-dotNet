2cfa415a8b4f074b189eaf038f6afa2c61ff7c57
Leandro <leandro.almeida@uvvnet.com.br>
2023-07-03 14:54:47 -0300

    implementando o contexto da aplicação

---
+++ CleanArchMVC.Infra/Context/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
