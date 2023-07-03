de66ff76f38916ea62b39e087a516fd3ac52dad8
Leandro <leandro.almeida@uvvnet.com.br>
2023-07-03 16:11:34 -0300

    registrando os serviços do contexto e do repositório.

---
+++ CleanArchMVC.Ioc/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.Infra.Context;
+using CleanArchMVC.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Ioc
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AdicionarInfraEstrutura(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), 
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+            return services;
+        }
+    }
+}
--- CleanArchMVC.WebUI/Startup.cs
+++ CleanArchMVC.WebUI/Startup.cs
@@ -1,3 +1,4 @@
+using CleanArchMVC.Ioc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,8 @@ namespace CleanArchMVC.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AdicionarInfraEstrutura(Configuration);
+
             services.AddControllersWithViews();
         }
 
