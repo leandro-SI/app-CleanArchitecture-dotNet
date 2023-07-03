3bbb081eace6294f31c7f6850230c635afc2b0d6
Leandro <leandro.almeida@uvvnet.com.br>
2023-07-03 15:10:41 -0300

    mapeamento das entidades com Fluent API

---
+++ CleanArchMVC.Infra/EntitiesConfiguration/CategoriaConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.EntitiesConfiguration
+{
+    public class CategoriaConfiguration : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Nome).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Categoria(1, "Material Escolar"),
+                new Categoria(2, "Eletrônicos"),
+                new Categoria(3, "Acessórios")
+            );
+
+        }
+    }
+}
---
+++ CleanArchMVC.Infra/EntitiesConfiguration/ProdutoConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.EntitiesConfiguration
+{
+    public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Nome).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Descricao).HasMaxLength(200).IsRequired();
+
+            builder.Property(p => p.Preco).HasPrecision(10,2);
+
+            builder.HasOne(p => p.Categoria).WithMany(c => c.Produtos)
+                .HasForeignKey(p => p.CategoriaId);
+        }
+    }
+}
